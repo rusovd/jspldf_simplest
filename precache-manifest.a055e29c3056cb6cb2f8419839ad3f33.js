28c0c6de689b25411e5167797f07a040c25c5242
Denys Rusov <d.rusov@svitla.com>
2020-08-28 16:40:29 +0300

    Updates

---
+++ precache-manifest.a055e29c3056cb6cb2f8419839ad3f33.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6272884bc8eb9b2908df9b4a85e213",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae2c33405fdbc73102c0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3adc3cb7c18796244e86",
+    "url": "/static/js/2.93a59f9e.chunk.js"
+  },
+  {
+    "revision": "1db169f4c5c3f2d515eb",
+    "url": "/static/js/3.63a36de1.chunk.js"
+  },
+  {
+    "revision": "10d58e813f7f36224398890b7073088c",
+    "url": "/static/js/3.63a36de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48a1614498327fd30782",
+    "url": "/static/js/4.8f0c7c12.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/static/js/4.8f0c7c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6c6a4e9e4093b12fd9",
+    "url": "/static/js/5.d3b6a099.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.d3b6a099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2c33405fdbc73102c0",
+    "url": "/static/js/main.ba94b4ef.chunk.js"
+  },
+  {
+    "revision": "5d59050412a03b6c84a8",
+    "url": "/static/js/runtime-main.15383e3e.js"
+  }
+]);
